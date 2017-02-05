d1aec3504e5057c7dc43096c128bf6211b839ae6
Jesse VanDerPol <jesse@pocketcircuit.com>
2017-02-04 23:06:16 -0600

    Fixed placeholder, added gitignore, and added provision files.

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -32,7 +32,7 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         let recordingName = "recordedVoice.wav"
         let pathArray = [dirPath, recordingName]
         let filePath = URL(string: pathArray.joined(separator: "/"))
-        print(filePath ?? <#default value#>)
+        print(filePath)
         
         let session = AVAudioSession.sharedInstance();
         try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
